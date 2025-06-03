2b89aa4db7931e03126082258625c8272182fd9e
Muhoti <strongmuhoti@gmail.com>
2025-06-03 12:44:08 +0300

    dropdowns for leakage and sewer burst

--- lib/components/Utils.dart
+++ lib/components/Utils.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 
 String getUrl() {
   // return "http://192.168.1.136:3003/api/";
-    // return "http://161.97.169.110:1043/api/";
+    return "http://161.97.169.110:1043/api/";
 
 
-  return "https://api-utilitymanager.mawasco.co.ke/api/";
+  // return "https://api-utilitymanager.mawasco.co.ke/api/";
 }
 
 Map<String, dynamic> parseJwt(String token) {
