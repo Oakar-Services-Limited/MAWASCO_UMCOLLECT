2fd4dd7d7d4c66433e0c6a3cffc07a04c4f72d3e
Muhoti <strongmuhoti@gmail.com>
2025-07-03 12:22:57 +0300

    Update API URL in Utils.dart and increment appVersion to 1.0.0 in stafflogin.dart for consistency with recent versioning changes.

--- lib/pages/stafflogin.dart
+++ lib/pages/stafflogin.dart
@@ -290,7 +290,7 @@ class _StaffLoginState extends State<StaffLogin> {
           'email': email,
           'password': password,
           'type': 'Mobile',
-          'appVersion': '0.0.0'
+          'appVersion': '1.0.0'
         }),
       );
 
