003632d501a73c517af24329fc94c4f36cd68608
Muhoti <strongmuhoti@gmail.com>
2025-11-24 10:09:33 +0300

    Increment version to 2.0.0+58 and update app version in staff login
    - Updated version number in pubspec.yaml and flutterVersionCode in build.gradle for the next release.
    - Changed appVersion in stafflogin.dart to reflect the new versioning, improving consistency across the application.

--- lib/pages/stafflogin.dart
+++ lib/pages/stafflogin.dart
@@ -287,7 +287,7 @@ class _StaffLoginState extends State<StaffLogin> {
           'email': email,
           'password': password,
           'type': 'Mobile',
-          'appVersion': '10.0.0'
+          'appVersion': '11.0.0'
         }),
       );
 
