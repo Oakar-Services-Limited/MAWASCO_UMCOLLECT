45e96b4ad8372a0667382ef9677a12bcf33ff7ac
Joycekinyanjui <joycekinyanjui364@gmail.com>
2026-05-19 12:05:19 +0300

    Update version numbers to 2.0.0+85 in pubspec.yaml and build.gradle, and increment appVersion in stafflogin.dart to '27'. Added new asset for Meter Replacement Exercise and updated dependencies in pubspec.lock for compatibility.

--- lib/components/Utils.dart
+++ lib/components/Utils.dart
@@ -4,9 +4,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 String getUrl() {
-  return "http://192.168.1.121:3003/api/";
+  // return "http://192.168.1.121:3003/api/";
   // return "http://192.168.1.136:3003/api/";
-  // return "https://api-utilitymanager.mawasco.co.ke/api/";
+  return "https://api-utilitymanager.mawasco.co.ke/api/";
 }
 
 Map<String, dynamic> parseJwt(String token) {
