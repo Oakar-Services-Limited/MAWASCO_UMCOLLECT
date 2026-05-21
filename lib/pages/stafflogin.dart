19ac0b54d4592e5d151d535b84afa6cd544a0b46
Joycekinyanjui <joycekinyanjui364@gmail.com>
2026-05-21 10:05:40 +0300

    upgraded app versions from 27 to 28

--- lib/pages/stafflogin.dart
+++ lib/pages/stafflogin.dart
@@ -287,7 +287,7 @@ class _StaffLoginState extends State<StaffLogin> {
           'email': email,
           'password': password,
           'type': 'Mobile',
-          'appVersion': '27'
+          'appVersion': '28'
         }),
       );
 
